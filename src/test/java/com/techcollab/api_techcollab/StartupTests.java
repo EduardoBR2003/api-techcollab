b9695b824aa466179a3a1e586f4c16e0de759205
EduardoBR2003 <sazogamer2003@gmail.com>
2025-05-20 09:06:07 -0300

    fist commit

---
+++ src/test/java/com/techcollab/api_techcollab/StartupTests.java
@@ -0,0 +1,13 @@
+package com.techcollab.api_techcollab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartupTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
